0093755cff73e025c80257352bb1c6aeb08c04e4
Jacob R <jakomania@gmail.com>
2022-05-10 21:10:36 +0200

    Añadido Doc Swagger

--- src/main/java/com/app/paracasa/SecurityConfig.java
+++ src/main/java/com/app/paracasa/SecurityConfig.java
@@ -45,7 +45,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		
+//		http.cors().and().csrf().disable();
 		http.csrf().disable().authorizeRequests()
+				.antMatchers("/swagger-ui/**", "/javainuse-openapi/**").permitAll()
 				.antMatchers("/api/tipos/**").hasAnyAuthority("ADMIN")
 				.anyRequest().authenticated()
 				.and().httpBasic();
